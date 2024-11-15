e30d02295e4f1de414ba179440c4aeccefa0df82
Valacor <valyrious.informatics@gmail.com>
2024-11-15 21:47:06 +0100

    starting of with binary trees

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace BinaryTree;
+
+// Binary trees are commonly used in computer science for efficient storage and retrieval of data
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TreeNode node1 = new TreeNode(1, null, new TreeNode(2, new TreeNode(3)));
+
+        var result1 = InorderTraversal(node1);
+        foreach (var item in result1)
+        {
+            Console.Write($"{item},");
+        }
+        
+        Console.WriteLine();
+        
+        TreeNode node2 = new TreeNode(1, 
+            new TreeNode(2, new TreeNode(4), new TreeNode(5, new TreeNode(6), new TreeNode(7))),
+            new TreeNode(3, null, new TreeNode(8, new TreeNode(9))));
+
+        var result2 = InorderTraversal(node2);
+        foreach (var item in result2)
+        {
+            Console.Write($"{item},");
+        }
+        
+        Console.WriteLine();
+        
+        var result3 = InorderTraversal(null);
+        Console.WriteLine(result3.Count == 0);
+        
+        var result4 = InorderTraversal(new TreeNode(1));
+        foreach (var item in result4)
+        {
+            Console.Write($"{item},");
+        }
+
+    }
+
+    public static IList<int> InorderTraversal(TreeNode root)
+    {
+        if (root == null)
+        {
+            return new List<int>();
+        }
+
+        IList<int> left = new List<int>();
+        IList<int> right = new List<int>();
+        
+        if (root.left is not null)
+        {
+            left = InorderTraversal(root.left);
+        }
+
+        if (root.right is not null)
+        {
+            right = InorderTraversal(root.right);
+        }
+
+        return [..left, root.val, ..right];
+    }
+    
+}
+
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right; 
+    
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) 
+    { 
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
